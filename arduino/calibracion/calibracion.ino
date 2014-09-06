0350702babbdf51a73f9c2ce7d963554cabb0b54
Juan Alonso <juan.alonso@tecnilogica.com>
2014-09-06 20:26:58 +0200

    Refactoring del código. Se quita el timestamp del log. Se añade el sensor de presencia. Se añaden dos leds: presencia y funcionamiento.

--- arduino/calibracion/calibracion.ino
+++ arduino/calibracion/calibracion.ino
@@ -7,6 +7,11 @@
 #define LEPIN 10
 #define OEPIN 9
 
+#define METHANEPIN 0
+#define PIRPIN 7
+#define LEDPIN 6
+#define BEACONPIN 5
+
 alphaNumeric myDisplay(SDIPIN, CLKPIN, LEPIN, OEPIN, NUMBER_OF_DISPLAYS);
 
 char buf [5];
@@ -17,17 +22,25 @@ void setup() {
   FileSystem.begin();
 
   Serial.begin(9600);
+  
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(BEACONPIN, OUTPUT);
+  //digitalWrite(LEDPIN, HIGH);
 
 }
 
 void loop () {
 
   String dataString;
-  dataString += getTimeStamp();
-  dataString += " = ";
+  //dataString += getTimeStamp();
+  //dataString += " = ";
 
-  int sensor = analogRead(0);
-  dataString += String(sensor);
+  int methaneSensor = analogRead(METHANEPIN);
+  dataString += String(methaneSensor) + " ";
+  
+  int pirSensor = digitalRead(PIRPIN);
+  digitalWrite(LEDPIN, pirSensor);
+  dataString += pirSensor == HIGH ? 1 : 0;
 
   // The FileSystem card is mounted at the following "/mnt/FileSystema1"
   File dataFile = FileSystem.open("/mnt/sd/datalog.txt", FILE_APPEND);
@@ -40,27 +53,31 @@ void loop () {
     Serial.println("error opening datalog.txt");
   }
   
-  sprintf (buf, "%04i", sensor);
+  sprintf (buf, "%04i", methaneSensor);
   myDisplay.scroll(buf, 0);
 
-  delay(1000);
+  digitalWrite(BEACONPIN, HIGH);
+  delay(50);
+  digitalWrite(BEACONPIN, LOW);
+  delay(950);
 
 }
 
-String getTimeStamp() {
-  String result;
-  Process time;
-  time.begin("date");
-  time.addParameter("+%F %T");
-  time.run();
 
-  while (time.available() > 0) {
-    char c = time.read();
-    if (c != '\n')
-      result += c;
-  }
-
-  return result;
-}
+//String getTimeStamp() {
+//  String result;
+//  Process time;
+//  time.begin("date");
+//  time.addParameter("+%F %T");
+//  time.run();
+//
+//  while (time.available() > 0) {
+//    char c = time.read();
+//    if (c != '\n')
+//      result += c;
+//  }
+//
+//  return result;
+//}
 
 
